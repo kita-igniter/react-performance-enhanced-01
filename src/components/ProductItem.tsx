b105bd8f207d7065eb1971f4f333b1210514651f
Kita Igniter <smtecsystem.469@gmail.com>
2022-12-01 07:07:52 -0300

    useCallback

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -5,13 +5,15 @@ interface ProductItemProps {
     id: number;
     price: number;
     title: string;
-  }
+  },
+  onAddToWishList: (id: number) => void; 
 }
 
-export function ProductItemComponent({ product }: ProductItemProps) {
+export function ProductItemComponent({ product, onAddToWishList}: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
+      <button onClick={() => onAddToWishList(product.id)}>Add to wishlist</button>
     </div>
   );
 }
